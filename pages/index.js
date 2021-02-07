237b3cfe96c6b577616f4833a5652f5624f41452
cjtheone <commit@cjtheone.net>
2021-02-07 21:49:21 +0900

    Delete unused function

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,7 @@
 import Layout from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import {postsTitle} from './posts/index'
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
 
 export default function Home() {
   return (
